a7e16c02e711da78b0627881fbd71c18edcb33ed
Minh T Nguyen <yourname@example.com>
2024-04-26 17:10:43 -0400

    Add unit tests for getRatio, getDataPoint

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote(['top_ask']['price'], (quote['top_bid']["price"] + quote['top_ask']['price'])/2)))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,27 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote(['top_ask']['price'], (quote['top_bid']["price"] + quote['top_ask']['price'])/2)))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio(self):
+    A = 0
+    B = 1
+    self.assertEqual(getRatio(A,B),0)
+
+    A = 1
+    B = 0
+    self.assertEqual(getRatio(A,B),None)
+
+    A = 0
+    B = 0
+    self.asserEqual(getRatio(A,B), None)
+    
+    A = 3
+    B = 2
+    self.assertEqual(getRatio(A,B),1.5)
+
 
 
 
